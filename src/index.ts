b6074cbfcdc543bf31ec0d63ed2c09240f00c62b
Muhammad Anas <99168538+MuhammadAnus5081@users.noreply.github.com>
2025-07-18 12:32:39 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import currencyRoutes from "./routes/currency";
-
+import serverless from 'serverless-http';
 dotenv.config();
 
 const app = express();
@@ -12,4 +12,6 @@ app.use(express.json());
 app.use("/api", currencyRoutes);
 
 // Instead of app.listen(), export the app for Vercel
-export default app;
+//app.listen(3000, () => console.log("Server running"));
+
+export default serverless(app);
